1b28a2c9071f112aac893576375b15d0b9e4df73
Casey Echternacht <casechtern@yahoo.com>
2024-07-25 13:43:14 -0600

    contrasted firstKey()/lastKey() with firstEntry()/lastEntry()

--- src/main/java/org/example/ExampleTreeMapTwo.java
+++ src/main/java/org/example/ExampleTreeMapTwo.java
@@ -1,2 +1,25 @@
-package org.example;public class ExampleTreeMapTwo {
+package org.example;
+
+import java.util.TreeMap;
+
+public class ExampleTreeMapTwo {
+	public static void main(String[] args) {
+		TreeMap<String, Integer> numbers = new TreeMap<>();
+		numbers.put("First", 1);
+		numbers.put("Second", 2);
+		numbers.put("Third", 3);
+		System.out.println("TreeMap: " + numbers);
+
+		//Using the firstKey() method
+		String firstKey = numbers.firstKey();
+		System.out.println("First Key; " + firstKey);
+		//lastKey()
+		String lastKey = numbers.lastKey();
+		System.out.println("Last Key; " + lastKey);
+
+		// Using firstEntry() method
+		System.out.println("First Entry: " + numbers.firstEntry());
+		//lastEntry()
+		System.out.println("Last Entry: " + numbers.lastEntry());
+	}
 }
